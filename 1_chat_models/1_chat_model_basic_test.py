4e373881b49919ec9fd5f3f82171d5ffb6d016e8
Harkirat <harkirat.makkar@gmail.com>
2025-01-10 07:24:09 +0530

    Added some code

--- 1_chat_models/1_chat_model_basic_test.py
+++ 1_chat_models/1_chat_model_basic_test.py
@@ -2,6 +2,7 @@
 # OpenAI Chat Model Documents: https://python.langchain.com/v0.2/docs/integrations/chat/openai/
 
 from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
 
 # Load environment variables from .env
@@ -10,9 +11,14 @@ load_dotenv()
 # Create a ChatOpenAI model
 model = ChatOpenAI(model="gpt-4o")
 
+messeges = [
+    SystemMessage("You are the best math problem solver"),
+    HumanMessage("What is 2 + 3 - 5 * 9"),
+    AIMessage("Using BODMAS, we solve multiply first which gives us 2 + 3 - 45. Then we solve addition \
+              which gives us 5 - 45. So, the final result is -40."),
+    HumanMessage("What is 6 - 8 + 9 - 5 * 3 / 4")
+]
 # Invoke the model with a message
-result = model.invoke("What is 81 divided by 9?")
-print("Full result:")
-print(result)
-print("Content only:")
+result = model.invoke(messeges)
 print(result.content)
+
